497635b08209cfa9bcbdfa644a83d1765339f548
unknown <agustin.fidelio@gmail.com>
2022-10-02 21:42:42 -0300

    formulario corregido responsive

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -7,8 +7,10 @@ export default function LandingPage() {
 		<div className={styles.principal}>
 			<div className={styles.containerText}> 
 				<h1  className={styles.henryFoodText}>Henry Food</h1>
-				<h1  className={styles.textoP}>● + 100 Recetas</h1>
-				<h1  className={styles.textoP}>● 11 Tipos de Dietas</h1>
+				<div className={styles.containertextosP}>
+					<h1  className={styles.textoP}>● + 100 Recetas</h1>
+					<h1  className={styles.textoP}>● 11 Tipos de Dietas</h1>
+				</div>
 				<div className={styles.containerButton}>
 					<Link to ='/home'>
 						<button className={styles.boton}>Igresar</button>
